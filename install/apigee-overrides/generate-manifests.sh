604fcdd0be811277f0840ab7f25be344822d9fe9
srinandan <nandanks@gmail.com>
2020-12-28 21:46:55 -0800

    utility to generate apigee manifests

--- install/apigee-overrides/generate-manifests.sh
+++ install/apigee-overrides/generate-manifests.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
-export OVERRIDES=overrides-srinandans-hybrid.yaml
-
+export OVERRIDES=overrides/overrides-srinandans-hybrid.yaml
 export ORG=$(cat $OVERRIDES | yq -r '.org')
 
-apigeectl init -f $OVERRIDES --dry-run=true --print-yaml >> apigee-init.yaml
+apigeectl init -f $OVERRIDES --dry-run=client --print-yaml >> apigee-controller.yaml
 
-components=($ORG "datastore" "telemetry")
-len=$( ${#components[@]} )
-for (( i=0; i<=$len; i++ ));
+components=("org" "datastore" "telemetry")
+len=${#components[@]}
+for (( i=0; i<$len; i++ ));
 do
-apigeectl apply -f $OVERRIDES --${i} --dry-run --print-yaml >> apigee-${i}.yaml
+apigeectl apply -f $OVERRIDES --${components[$i]} --dry-run=client --print-yaml >> apigee-${components[$i]}.yaml
 done
 
 
-cat $OVERRIDES | yq -r '.envs | .[].name' | while read -r a; do apigeectl apply -f $OVERRIDES --env $a --dry-run=client --print-yaml >> apigee-srinandans-hybrid-$a; done
+cat $OVERRIDES | yq -r '.envs | .[].name' | while read -r a; do apigeectl apply -f $OVERRIDES --env $a --dry-run=client --print-yaml >> apigee-srinandans-hybrid-$a.yaml; done
 
-apigeectl apply -f $OVERRIDES --settings=virtualhosts >> apigee-virtualhosts.yaml
+apigeectl apply -f $OVERRIDES --settings=virtualhosts --dry-run=client --print-yaml >> apigee-virtualhosts.yaml
+
+mv apigee-org.yaml apigee-$ORG.yaml
